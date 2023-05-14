f56160fc0b200d31c43fa6f059f64159acd3ffbc
Zheng Lu <zhenglu@dhcp-v107-191.mobile.uci.edu>
2023-05-13 20:39:57 -0700

    test nav's title

--- test_pytest_1.py
+++ test_pytest_1.py
@@ -20,8 +20,8 @@ class Test_URL_Chrome(BasicTest):
         assert expected_title == self.driver.title
 
     def test_nav_home_title(self):
-        self.driver.get('file:///Users/zhenglu/Documents/CS297P/Frontend/betting-site/login.html')
-        self.driver.maximize_window()
+        #self.driver.get('file:///Users/zhenglu/Documents/CS297P/Frontend/betting-site/login.html')
+        #self.driver.maximize_window()
         
         self.driver.find_element(By.CLASS_NAME, "nav-link").click()
         heading_element = self.driver.find_element(By.CLASS_NAME, "heading")
@@ -31,12 +31,45 @@ class Test_URL_Chrome(BasicTest):
         assert expected_title == heading_element.text
     
     def test_nav_bet_title(self):
-        self.driver.get('file:///Users/zhenglu/Documents/CS297P/Frontend/betting-site/login.html')
-        self.driver.maximize_window()
+        #self.driver.get('file:///Users/zhenglu/Documents/CS297P/Frontend/betting-site/login.html')
+        #self.driver.maximize_window()
         
         self.driver.find_element(By.XPATH, "/html/body/header/nav/div/ul[1]/li[2]").click()
         heading_element = self.driver.find_element(By.CLASS_NAME, "heading")
         
         expected_title = "The Games that can bet for now"
+
+        assert expected_title == heading_element.text
+    
+    def test_nav_history_title(self):
+        #self.driver.get('file:///Users/zhenglu/Documents/CS297P/Frontend/betting-site/login.html')
+        #self.driver.maximize_window()
+        
+        self.driver.find_element(By.XPATH, "/html/body/header/nav/div/ul[1]/li[3]").click()
+        heading_element = self.driver.find_element(By.CLASS_NAME, "heading")
+        
+        expected_title = "Purchase History"
+
+        assert expected_title == heading_element.text
+
+    def test_nav_aboutus_title(self):
+        #self.driver.get('file:///Users/zhenglu/Documents/CS297P/Frontend/betting-site/login.html')
+        #self.driver.maximize_window()
+        
+        self.driver.find_element(By.XPATH, "/html/body/header/nav/div/ul[1]/li[4]").click()
+        heading_element = self.driver.find_element(By.CLASS_NAME, "heading")
+        
+        expected_title = "About Our Team"
+
+        assert expected_title == heading_element.text
+    
+    def test_nav_contactus_title(self):
+        #self.driver.get('file:///Users/zhenglu/Documents/CS297P/Frontend/betting-site/login.html')
+        #self.driver.maximize_window()
+        
+        self.driver.find_element(By.XPATH, " /html/body/header/nav/div/ul[1]/li[5]").click()
+        heading_element = self.driver.find_element(By.CLASS_NAME, "heading")
+        
+        expected_title = "Contact Us"
 
         assert expected_title == heading_element.text
